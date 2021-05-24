660baee726775a74714be55967c3a6eb19359713
Heather Stratton <hastratton@gmail.com>
2021-05-24 14:19:55 -0400

    mod css, add scroll to home

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 //const csrf = require('csurf');
 //const flash = require('connect-flash');
 const cors = require('cors');
-//const PRIVATE = require('./private');
+const PRIVATE = require('./private');
 const corsOptions = {
   origin: "https://ta03-cse341.herokuapp.com/",
   optionsSuccessStatus: 200
