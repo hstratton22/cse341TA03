e1d56fe45124c9c8c8a9dd67b8d3b23e48e903ea
Heather Stratton <hastratton@gmail.com>
2021-06-26 12:27:35 -0400

    extra input for avenger

--- public/scripts/ta10.js
+++ public/scripts/ta10.js
@@ -8,8 +8,12 @@ const populateList = () => {
 
             for (const avenger of data.avengers) {
                 const li = document.createElement('li');
-                li.appendChild(document.createTextNode(avenger.name));
+                //const liAKA =document.createElement('li');
+                //liAKA.appendChild(document.createTextNode("AKA " + avenger.AKA));
+                // 
+                li.appendChild(document.createTextNode(avenger.name + " AKA " + avenger.AKA));
                 nameList.appendChild(li);
+                //nameList.appendChild(liAKA);
             }
         })
         .catch(err => {
@@ -18,19 +22,28 @@ const populateList = () => {
 }
 const submitName = () => {
     const newName = document.getElementById('newName').value;
+    const newNameAlias = document.getElementById('alias').value;
+    var result = { newName: newName, alias: newNameAlias};
+    var resultJson = JSON.stringify(result);
+    console.log("ResultJson", resultJson);
+    console.log("result", result);
     fetch('ta10/insertName', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ newName })
+        body: JSON.stringify( result )
+        //body: JSON.stringify({ newName })
+        //body: JSON.stringify({ newName, newNameAlias })
     })
         .then(res => {
             document.getElementById('newName').value = '';
+            document.getElementById('alias').value = '';
             populateList();
         })
         .catch(err => {
             document.getElementById('newName').value = '';
+            document.getElementById('alias').value = '';
             console.error(err);
         })
 }
--- routes/teamRoutes/ta10.js
+++ routes/teamRoutes/ta10.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 // Path to your JSON file, although it can be hardcoded in this file.
-const dummyData = require('../../data/ta10-data.json');
+const dummyData = require('../../data/ta10-data2.json');
 router.use(bodyParser.json());
 
 router.get('/fetchAll', (req, res, next) => {
@@ -12,10 +12,16 @@ router.get('/fetchAll', (req, res, next) => {
 });
 
 router.post('/insertName', (req, res, next) => {
-    if (req.body.newName !== undefined) {
-        const newName = req.body.newName;
+    console.log("req.body inside /insertName", req.body);
+    console.log("newName", req.body.newName);
+    console.log("alias", req.body.alias);
+    
+    
+    if (req.body.newName !== undefined) {//} && req.body.alias !== undefined) {
+       const newName = req.body.newName;
+        const alias = req.body.alias || "undefined";
         if (!dummyData.avengers.some(a => a.name === newName)) {
-            dummyData.avengers.push({ name: newName });
+            dummyData.avengers.push({ name: newName, AKA: alias });
             res.sendStatus(200);
         }
 
